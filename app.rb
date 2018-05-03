dc54ae16105701d045173e57e95d20cb3a5b7ab9
itggot-Jacob-Tellvik <Jacob.tellvik@itggot.se>
2018-05-03 10:29:47 +0200

    nytt

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 class App < Sinatra::Base
 
 	get '/' do
-		"Hello, Grillkorv!"
+		erb(:index)
 	end
 
+	post '/login' do
+		username = params[:username]
+		password = params[:password]
+
+		db = SQLite3::Database.new("db/databas.sqlite3")
+		
+	end
 end           
